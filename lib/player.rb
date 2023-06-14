34950cc22265b38eb4845b2eca2d11d77846cccd
Won Rhim <wwrk22@gmail.com>
2023-06-13 21:23:27 -0400

    Write first unit test for Player#prompt_move

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,17 @@
 require_relative './standards/pieces'
+require_relative './move/syntax_validator'
 
 class Player
-
   attr_reader :name, :color
 
   def initialize(name, color)
     @name = name
     @color = color
+    @syn_vtor = SyntaxValidator.new
   end
 
   def prompt_move
+    input = gets.chomp
+    return { move: input, color: @color } if @syn_vtor.validate(input)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe Player do
   describe '#prompt_move' do
     context "when player color is white" do
       context "when move syntax is valid" do
-        subject(:white_valid) { described_class.new('Foo', 'white')
+        subject(:white_valid) { described_class.new('Foo', Pieces::WH) }
+        let(:valid_move) { 'a3' }
+        let(:syn_vtor) { instance_double(SyntaxValidator) }
 
         it "returns a hash of the move and the player's color" do
+          allow(white_valid).to receive(:gets).and_return(valid_move)
+          allow(syn_vtor).to receive(:validate).and_return(true)
+          white_valid.instance_variable_set(:@syn_vtor, syn_vtor)
+
+          exp_out = { move: valid_move, color: Pieces::WH }
+          expect(white_valid.prompt_move).to eq(exp_out)
         end
       end
 
