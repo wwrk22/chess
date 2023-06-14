d9caebe924aa3a3e1d09554442127c598dacbfd5
Won Rhim <wwrk22@gmail.com>
2023-06-14 17:11:50 -0400

    Add bishop valid move syntax regex

---
+++ lib/move/syntax/bishop_moves.rb
@@ -0,0 +1,7 @@
+module Move
+  module Syntax
+    module BishopMoves
+      MOVE = /^B([a-h][1-8])?x?(?!\1)[a-h](?!\1)[1-8]$/
+    end
+  end
+end
---
+++ to-do.md
@@ -0,0 +1,1 @@
+Fix regex for Bishop and Knight move syntax using lookaround patterns
