83fe19595d5bd5337f686a85e39d08d377245683
Won Rhim <wwrk22@gmail.com>
2023-06-14 14:19:57 -0400

    Add missing part to rook move valid syntax regex; add unit tests

--- lib/move/syntax/rook_moves.rb
+++ lib/move/syntax/rook_moves.rb
@@ -1,7 +1,7 @@
 module Move
   module Syntax
     module RookMoves
-      MOVE = /^R[a-h][1-8]$/
+      MOVE = /^R[a-h, 1-8]?[a-h][1-8]$/
       CAPTURES = [/^Rx[a-h][1-8]$/, /^R[a-h, 1-8]x[a-h][1-8]$/]
     end
   end
