0dd1ad12d4d20d840a414b0efa697cc869db7bff
Won Rhim <wwrk22@gmail.com>
2023-06-15 11:25:20 -0400

    Raise error for unknown color

--- lib/move/syntax/bishop_validator.rb
+++ lib/move/syntax/bishop_validator.rb
@@ -1,9 +1,14 @@
 require_relative './bishop_moves'
+require './lib/errors/color_unknown_error'
 
 module Move
   module Syntax
     class BishopValidator
       def validate(move)
+        if move[:color] != Piece::WH && move[:color] != Piece::BL
+          raise ColorUnknownError.new(move[:color])
+        end
+
         move if move[:move] =~ BishopMoves::MOVE
       end
     end
