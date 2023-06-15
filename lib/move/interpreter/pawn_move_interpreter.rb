4dd90c47e6cac9887ace3e7a9fd8a01fe3473292
Won Rhim <wwrk22@gmail.com>
2023-06-15 15:54:34 -0400

    Refactor #interpret_move - Move class should be set to a non-capture move by default.

--- lib/move/interpreter/pawn_move_interpreter.rb
+++ lib/move/interpreter/pawn_move_interpreter.rb
@@ -3,26 +3,15 @@ require './lib/standards/piece'
 
 class PawnMoveInterpreter
   
-  # Takes the raw format of a move (e.g. { move: 'a3', color: Piece::WH }, and
-  # interprets it to construct then return a Move object.
-  # The raw move should have had its syntax and color validated by a validator
-  # before being passed to this interpreter.
-  def interpret_move(raw_move)
-    interpreted_move = Move.new
-
-    interpreted_move.color = raw_move[:color]
-
-    if interpreted_move.color == Piece::WH
-      interpreted_move.opp_color = Piece::BL
-    else
-      interpreted_move.opp_color = Piece::WH
-    end
-
-    interpreted_move.piece_type = Piece::PA
-
-    #interpreted_move.target_sq = parse_target_sq(raw_move[:move])
-
-    #compute_possible_start_sqs(interpreted_move)
+  # Takes the raw format of a non-capture move
+  # (e.g. { move: { file: 'a', rank: 3 }, color: Piece::WH }), and interprets
+  # it to construct then return a Move object. The raw move should have had its
+  # syntax and color validated by a validator before being passed to this
+  # interpreter.
+  def interpret_move(raw_move, interpreted_move = Move.new)
+    set_piece_info(interpreted_move, raw_move[:color])
+    compute_possible_start_sqs(raw_move[:move], raw_move[:color])
+    interpreted_move.target_sq = raw_move[:move]
   end
 
   def compute_possible_start_sqs(target_sq, color, sqs = [])
@@ -33,6 +22,12 @@ class PawnMoveInterpreter
 
 
   private
+
+  def set_piece_info(interpreted_move, color)
+    interpreted_move.piece_type = Piece::PA
+    interpreted_move.color = color
+    interpreted_move.opp_color = (color == Piece::WH) ? Piece::BL : Piece::WH
+  end
 
   def compute_possible_start_sqs_wh(target_sq, sqs)
     sqs << { file: target_sq[:file], rank: target_sq[:rank] - 1 }
--- lib/move/move.rb
+++ lib/move/move.rb
@@ -2,4 +2,9 @@ class Move
   attr_accessor :possible_start_sqs, :start_sq, :target_sq
   attr_accessor :piece_type, :color, :opp_color
   attr_accessor :capture, :en_passant
+
+  def initialize
+    @capture = false
+    @en_passant = false
+  end
 end
