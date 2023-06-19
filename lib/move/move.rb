34dafa02e6775603c19ca60ffaf38fc236e04f17
Won Rhim <wwrk22@gmail.com>
2023-06-19 17:19:43 -0400

    Remove unnecessary attributes

--- lib/move/move.rb
+++ lib/move/move.rb
@@ -1,10 +1,17 @@
 class Move
-  attr_accessor :starts, :start_sq, :target_sq
-  attr_accessor :piece_type, :color, :opp_color
-  attr_accessor :capture, :en_passant
+  # All the possible starting squares
+  attr_accessor :starts
+
+  # The destination square
+  attr_accessor :target
+
+  # Type and color of the chess piece being play
+  attr_accessor :piece, :color
+
+  # Tell whether or not the move is a capture
+  attr_accessor :capture
 
   def initialize
     @capture = false
-    @en_passant = false
   end
 end
