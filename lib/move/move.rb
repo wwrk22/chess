db19d8563af07b05a5dc7d70acf506fd45740f74
Won Rhim <wwrk22@gmail.com>
2023-06-22 20:22:13 -0400

    Remove all logic related to computing the possible starting squares of a move

--- lib/move/move.rb
+++ lib/move/move.rb
@@ -20,31 +20,4 @@ class Move
   def initialize
     @capture = false
   end
-
-  # Use all of the data in this Move except for @starts to compute all possible
-  # starting squares. A block that performs the actual computation is required
-  # to output an array of squares. If any of the necessary information is
-  # missing, thn return false. Otherwise, proceed to call the block, then
-  # return true.
-  def compute_starts
-    return nil if to_hash.has_value?(nil) || block_given? == false
-    yield to_hash(true)
-  end
-
-  private
-
-  # Return a hash of the attributes, all except @starts.
-  def to_hash(include_start = false)
-    if @piece == Piece::PA && @capture
-      { target: @target, piece: @piece, color: @color,
-        capture: @capture, start_f: @start_f }
-    else
-      if include_start
-        { target: @target, piece: @piece, color: @color, capture: @capture,
-          start_f: @start_f, start_r: @start_r }
-      else
-        { target: @target, piece: @piece, color: @color, capture: @capture }
-      end
-    end
-  end
 end
