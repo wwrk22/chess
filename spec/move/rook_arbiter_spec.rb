4f8e7b3f8a3eca24a12bbbdff3438e79e6b46961
Won Rhim <wwrk22@gmail.com>
2023-07-03 12:59:21 -0400

    Cover the case where the start square is NOT empty - Added unit tests to cover all remaining cases.

--- spec/move/rook_arbiter_spec.rb
+++ spec/move/rook_arbiter_spec.rb
@@ -89,15 +89,43 @@ RSpec.describe RookArbiter do
 
       context "when the one square is not empty" do
         context "when the chess piece on the square matches type and color" do
+          before do
+            allow(board).to receive(:at).with(file, rank).and_return({ type: test_type, color: test_color })
+          end
+
+          it "returns the square" do
+            expect(arbiter.check_start(board, start, wh_rook)).to eq(start)
+          end
         end # context "when the chess piece on the square matches type and color"
 
         context "when the chess piece on the square matches the type only" do
+          before do
+            allow(board).to receive(:at).with(file, rank).and_return({ type: test_type, color: Piece::BL })
+          end
+
+          it "returns nil" do
+            expect(arbiter.check_start(board, start, wh_rook)).to be_nil
+          end
         end # context "when the chess piece on the square matches the type only"
 
         context "when the chess piece on the square matches the color only" do
+          before do
+            allow(board).to receive(:at).with(file, rank).and_return({ type: Piece::PA, color: test_color })
+          end
+
+          it "returns nil" do
+            expect(arbiter.check_start(board, start, wh_rook)).to be_nil
+          end
         end # context "when the chess piece on the square matches the color only"
 
         context "when the chess piece on the square matches neither type nor color" do
+          before do
+            allow(board).to receive(:at).with(file, rank).and_return({ type: Piece::PA, color: Piece::BL })
+          end
+
+          it "returns nil" do
+            expect(arbiter.check_start(board, start, wh_rook)).to be_nil
+          end
         end # context "when the chess piece on the square matches neither type nor color"
       end # context "when the one square is not empty"
     end # context "when there is only one possibl start square"
