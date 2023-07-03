a962ae530982c831f9cbe9262a379fd4a79a5173
Won Rhim <wwrk22@gmail.com>
2023-07-03 19:26:21 -0400

    #check_start is to be refactored

--- lib/move/rook_arbiter.rb
+++ lib/move/rook_arbiter.rb
@@ -1,11 +1,4 @@
 class RookArbiter
-  def judge(board, data)
-    capture, starts, player_color = [data[:capture], data[:starts], data[:color]]
-    target = board.at(data[:target][:f], data[:target][:r])
-
-    return judge_capture(target, starts, player_color, board) if capture
-    return judge_non_capture(target, starts, player_color, board)
-  end
 
   # Return true if a chess piece specified by the type and color of the 'piece'
   # hash exists on the specified square of the board. Return false if the chess
@@ -27,14 +20,5 @@ class RookArbiter
     if start[:type] == piece[:type] && start[:color] == piece[:color]
       return square
     end
-  end
-
-  private
-
-  def judge_non_capture(target, possible_starts, player_color, board)
-    return nil if target.nil? == false
-  end
-
-  def judge_capture(target, possible_start, player_color, board)
   end
 end
