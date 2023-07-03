496fb00e7532c79b2a314db3c64d4004124c0ac6
Won Rhim <wwrk22@gmail.com>
2023-07-03 12:07:18 -0400

    Add a way to check if a square on the board holds a chess piece of certain type and color - Added a unit test to check the case of an empty square.

--- lib/move/rook_arbiter.rb
+++ lib/move/rook_arbiter.rb
@@ -7,11 +7,22 @@ class RookArbiter
     return judge_non_capture(target, starts, player_color, board)
   end
 
+  # Return true if a chess piece specified by the type and color of the 'piece'
+  # hash exists on the specified square of the board. Return false if the chess
+  # piece does not match or if the square is empty.
+  # The key-value pairs for the 'piece' hash should include the 'type' key with
+  # one of the chess piece types in the Piece module, and the 'color' key with
+  # either of the two chess piece colors in the Piece module.
+  def check_target(board, square, piece = {})
+    if board.at(square[:file], square[:rank]).nil?
+      return nil
+    end
+  end
+
   private
 
   def judge_non_capture(target, possible_starts, player_color, board)
     return nil if target.nil? == false
-
   end
 
   def judge_capture(target, possible_start, player_color, board)
--- spec/move/rook_arbiter_spec.rb
+++ spec/move/rook_arbiter_spec.rb
@@ -5,99 +5,21 @@ require './lib/standards/piece'
 RSpec.describe RookArbiter do
   subject(:arbiter) { described_class.new }
 
-  describe '#judge' do
-    context "when move is not a capture" do
-      context "when the target square is not empty" do
-        let!(:board) { instance_double(Board) }
+  describe '#check_target' do
+    context "when the target square on the board is empty" do
+      let!(:file) { 'd' }
+      let!(:rank) { 4 }
+      let!(:board) { instance_double(Board) }
 
-        before do
-          allow(board).to receive(:at).with('d', 4).and_return('some chess piece')
-        end
-
-        it "returns nil" do
-          data = { capture: false, target: { f: 'd', r: 4 } }
-          expect(arbiter.judge(board, data)).to be_nil
-        end
+      before do
+        allow(board).to receive(:at).with(file, rank).and_return(nil)
       end
 
-      context "when the target square is empty" do
-
-        context "when there is only one possible starting square" do
-
-          context "when there is a rook of the player color on the square" do
-            let!(:board) { instance_double(Board) }
-
-            before do
-              allow(board).to receive(:at).with('d', 4).and_return(nil)
-              allow(board).to receive(:at).with('a', 4).and_return({ type: Piece::RO, color: Piece::WH })
-            end
-
-            it "returns the one possible starting square" do
-              data = {
-                capture: false,
-                target: { f: 'd', r: 4 },
-                starts: [{ f: 'a', r: 4 }],
-                color: Piece::WH
-              }
-
-              expected = { f: 'a', r: 4 }
-              expect(arbiter.judge(board, data)).to eq(expected)
-            end
-          end
-
-          context "when there is not a rook of the player color on the square" do
-
-            context "when the square is empty" do
-
-            end
-
-            context "when the square is not empty" do
-
-            end
-
-          end # context "when there is not a rook of the player color on the square"
-
-        end # context "when there is only one possible starting square"
-
-        context "when there is more than one possible starting square" do
-
-          context "when the starting squares are specified by a file" do
-
-          end
-
-          context "when the starting squares are specified by a rank" do
-
-          end
-
-          context "when there is no file or rank specified" do
-
-          end
-
-        end # context "when there are more than one possible starting square"
-
-      end # context "when the target square is empty"
-
-    end # context "when move is not a capture"
-
-    context "when move is a capture" do
-
-      context "when the target square is empty" do
-
+      it "returns nil" do
+        target = { file: file, rank: rank }
+        piece = { type: Piece::RO, color: Piece::WH }
+        expect(arbiter.check_target(board, target, piece)).to be_nil
       end
-
-      context "when the target square is not empty" do
-        
-        context "when the target square holds an opponent's chess piece" do
-
-        end
-
-        context "when the target square does not hold an opponent's chess piece" do
-
-        end
-
-      end # context "when the target square is not empty"
-
-    end # context "when move is a capture"
-
-  end # describe '#judge'
+    end # context "when the target square on the board is empty"
+  end # describe '#check_target'
 end
