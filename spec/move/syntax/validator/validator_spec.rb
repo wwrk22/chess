8dfab0b3706a76cbb48914f1495c7733e9e4a770
Won Rhim <wwrk22@gmail.com>
2023-07-05 15:46:09 -0400

    Create a wrapper class that uses all of the validator classes - Unit tests added.

--- spec/move/syntax/validator/validator_spec.rb
+++ spec/move/syntax/validator/validator_spec.rb
@@ -8,15 +8,13 @@ RSpec.describe Move::Syntax::Validator::PieceTypeParser do
 
     context "when the move is for a pawn" do
       it "returns 'P' to indicate pawn" do
-        move = { move: 'a3', color: ChessPiece::WH }
-        expect(parser.parse(move)).to eq(ChessPiece::PA)
+        expect(parser.parse('a3')).to eq(ChessPiece::PA)
       end
     end
 
     context "when the move is for a rook" do
       it "returns 'R' to indicate rook" do
-        move = { move: 'Rd4', color: ChessPiece::WH }
-        expect(parser.parse(move)).to eq(ChessPiece::RO)
+        expect(parser.parse('Rd4')).to eq(ChessPiece::RO)
       end
     end
   end # #parse
@@ -26,10 +24,28 @@ end # Move::Syntax::Validator::PieceTypeParser
 
 RSpec.describe Move::Syntax::Validator do
   
-  describe '' do
+  describe '#validate' do
+    subject(:validator) { described_class.new }
 
+    context "when the move is for a pawn" do
+      it "returns the output of PawnValidator#validate" do
+        move = { move: 'a3', color: ChessPiece::WH }
+        allow(validator.instance_variable_get(:@parser)).to receive(:parse).and_return(ChessPiece::PA)
 
+        expect(validator.instance_variable_get(:@pawn_validator)).to receive(:validate).with(move)
+        validator.validate(move)
+      end
+    end
 
+    context "when the move is for a piece that is not pawn" do
+      it "returns the output of #validate of the appropriate validator class" do
+        move = { move: 'Nba3', color: ChessPiece::BL }
+        allow(validator.instance_variable_get(:@parser)).to receive(:parse).and_return(ChessPiece::KN)
+
+        expect(validator.instance_variable_get(:@knight_validator)).to receive(:validate).with(move)
+        validator.validate(move)
+      end
+    end
   end
 
 end
