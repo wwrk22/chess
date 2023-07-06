21a58b6aa0901f6a68791918aaf7b8356c8230c1
Won Rhim <wwrk22@gmail.com>
2023-07-06 13:29:15 -0400

    Indicate who is computing/interpreting which values

--- lib/move/move.rb
+++ lib/move/move.rb
@@ -1,20 +1,25 @@
 class Move
   # The type and color of the piece making the move.
+  # Interpreted by MoveInterpreter.
   attr_accessor :moving_piece
 
   # The target square that the @moving_piece is moving to or capturing.
+  # Interpreted by MoveInterpreter.
   attr_accessor :target
 
   # Indicate whether or not the move is a capture.
   # Format: Boolean
+  # Interpreted by MoveInterpreter.
   attr_accessor :capture
 
   # The possible starting squares of the moving piece used only when the moving
   # piece is a pawn or a knight.
+  # Computed by a move computer.
   attr_accessor :starts
 
   # The directions from @target that can be followed to check for possible
   # starting squares of @moving_piece. This is used by all piece types except
   # for pawn and knight.
+  # Computed by a move computer.
   attr_accessor :directions
 end
