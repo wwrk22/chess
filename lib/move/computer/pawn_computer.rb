17ce4497556fa154b576ca654566246d85cef69a
Won Rhim <wwrk22@gmail.com>
2023-07-06 13:43:39 -0400

    Add a method to compute the possible starting squares for a moving but non-capturing pawn - The method returns an array of either one or two starting squares. - Added a unit test for the case when a white pawn moves a single square.

--- lib/move/computer/pawn_computer.rb
+++ lib/move/computer/pawn_computer.rb
@@ -1,2 +1,30 @@
+require './lib/standard/chess_piece'
+
+
+# Computes the required information for use by the chess board in order to
+# perform a move.
 class PawnComputer
+  
+  # Compute at most two possible starting squares for a non-capture move.
+  def compute_non_capture(target, player_color, starts = [])
+    target_file, target_rank = [target[:file], target[:rank]]
+
+    # Single move
+    if player_color == ChessPiece::WH && rank_not_four?(target_rank)
+      start_file, start_rank = [target_file, target_rank - 1]
+      starts << { file: start_file, rank: start_rank }
+    end
+
+    # Double move
+
+
+    return starts
+  end
+
+
+  private
+
+  def rank_not_four?(rank)
+    rank != 4 && 1 < rank
+  end
 end
--- spec/move/computer/pawn_computer_spec.rb
+++ spec/move/computer/pawn_computer_spec.rb
@@ -3,4 +3,32 @@ require './lib/move/computer/pawn_computer'
 
 RSpec.describe PawnComputer do
 
+  describe '#compute_non_capture' do
+
+    context "when the move is not a pawn's first move in a game" do
+      
+      subject(:computer) { described_class.new }
+
+      it "computes the one starting square for the pawn" do
+
+        exp_output = [{ file: 'a', rank: 2 }]
+
+        target = { file: 'a', rank: 3 }
+
+        player_color = ChessPiece::WH
+
+        expect(computer.compute_non_capture(target, player_color)).to eq(exp_output)
+
+      end
+
+    end
+
+    context "when the move is a pawn's first move in a game" do
+
+      xit "computes the two possible starting squares for the pawn" do
+      end
+
+    end
+
+  end
 end
