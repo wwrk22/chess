8611a1c093adace8ed35b65161a3e886edb0934e
Won Rhim <wwrk22@gmail.com>
2023-07-14 11:01:22 -0400

    Refactor

--- lib/move/computer/bishop_computer.rb
+++ lib/move/computer/bishop_computer.rb
@@ -5,19 +5,25 @@ class BishopComputer
   include ChessBoard
   
   # Compute the starting square only if a valid starting file or rank is given.
-  def compute_move(target, start_place, starts = [])
-    if valid_file? start_place
-      file_diff = (target[:file].ord - start_place.ord).abs
-      starts << { file: start_place, rank: target[:rank] + file_diff }
-      starts << { file: start_place, rank: target[:rank] - file_diff }
-    end
+  def compute_move(target, start_place)
+    return compute_with_file(target, start_place) if valid_file? start_place
+    return compute_with_rank(target, start_place) if valid_rank? start_place
+  end
 
-    if valid_rank? start_place
-      rank_diff = (target[:rank] - start_place).abs
-      starts << { file: (target[:file].ord - rank_diff).chr, rank: start_place }
-      starts << { file: (target[:file].ord + rank_diff).chr, rank: start_place }
-    end
+  
+  private
 
+  def compute_with_file(target, start_file, starts = [])
+    file_diff = (target[:file].ord - start_file.ord).abs
+    starts << { file: start_file, rank: target[:rank] + file_diff }
+    starts << { file: start_file, rank: target[:rank] - file_diff }
+    starts
+  end
+
+  def compute_with_rank(target, start_rank, starts = [])
+    rank_diff = (target[:rank] - start_rank).abs
+    starts << { file: (target[:file].ord - rank_diff).chr, rank: start_rank }
+    starts << { file: (target[:file].ord + rank_diff).chr, rank: start_rank }
     starts
   end
 end
