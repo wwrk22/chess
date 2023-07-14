27afe4714e2c866cf7673361bcc1e937f6efc30a
Won Rhim <wwrk22@gmail.com>
2023-07-14 11:33:33 -0400

    Refactor

--- lib/move/computer/king_computer.rb
+++ lib/move/computer/king_computer.rb
@@ -6,17 +6,33 @@ class KingComputer
   
   # Compute all of the at most eight possible starting squares.
   def compute_move(target, starts = [])
-    left_file = (target[:file].ord - 1).chr
-    right_file = (target[:file].ord + 1).chr
-    rank = target[:rank] - 1
-
-    until valid_rank?(rank) == false do
-      starts << { file: left_file, rank: rank } if valid_file? left_file
-      starts << { file: target[:file], rank: rank } if rank != target[:rank]
-      starts << { file: right_file, rank: rank } if valid_file? right_file
-      rank += 1
-    end
-
+    add_left_valid_squares((target[:file].ord - 1).chr, target[:rank] - 1, starts)
+    add_mid_valid_squares(target[:file], target[:rank] - 1, target[:rank], starts)
+    add_right_valid_squares((target[:file].ord + 1).chr, target[:rank] - 1, starts)
     starts
+  end
+
+
+  private
+
+  def add_left_valid_squares(left_file, start_rank, starts)
+    until valid_rank?(start_rank) == false do
+      starts << { file: left_file, rank: start_rank } if valid_file? left_file
+      start_rank += 1
+    end
+  end
+  
+  def add_mid_valid_squares(mid_file, start_rank, target_rank, starts)
+    until valid_rank?(start_rank) == false do
+      starts << { file: mid_file, rank: start_rank } if start_rank != target_rank
+      start_rank += 1
+    end
+  end
+
+  def add_right_valid_squares(right_file, start_rank, starts)
+    until valid_rank?(start_rank) == false do
+      starts << { file: right_file, rank: start_rank } if valid_file? right_file
+      start_rank += 1
+    end
   end
 end
