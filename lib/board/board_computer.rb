e7ca01adc508438ddec0a74748864d8e701e3084
Won Rhim <wwrk22@gmail.com>
2023-07-14 18:49:57 -0400

    Add module for computing direction of a straight path between two squares

--- lib/board/board_computer.rb
+++ lib/board/board_computer.rb
@@ -1,8 +1,10 @@
 module BoardComputer
-  
   def compute_direction(a, b)
     file_diff = b[:file].ord - a[:file].ord
     rank_diff = b[:rank] - a[:rank]
-    { file: file_diff, rank: rank_diff }
+
+    if file_diff == 0 || rank_diff == 0 || file_diff.abs == rank_diff.abs
+      return { file: file_diff, rank: rank_diff }
+    end
   end
 end
--- spec/board/board_computer_spec.rb
+++ spec/board/board_computer_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe BoardComputer do
   describe '#compute_direction' do
     context "when the path is horizontal" do
       context "when going from left to right" do
-        context "when the ending square is on the path" do
-          it "returns the directions" do
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
             a = { file: 'a', rank: 1 }
             b = { file: 'h', rank: 1 }
-
             computed_direction = computer.compute_direction(a, b)
             correct_direction = { file: 7, rank: 0 }
 
@@ -19,23 +18,181 @@ RSpec.describe BoardComputer do
           end
         end
 
-        context "when the ending square is not on the path" do
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'a', rank: 1 }
+            b = { file: 'h', rank: 2 }
+            computed_direction = computer.compute_direction(a, b)
+            
+            expect(computed_direction).to be_nil
+          end
         end
-      end
+      end # context "when going from left to right"
 
       context "when going from right to left" do
-      end
-    end
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'h', rank: 1 }
+            b = { file: 'a', rank: 1 }
+            computed_direction = computer.compute_direction(a, b)
+            correct_direction = { file: -7, rank: 0 }
+
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'h', rank: 1 }
+            b = { file: 'a', rank: 2 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from right to left"
+    end # context "when the path is horizontal"
 
     context "when the path is vertical" do
       context "when going from top to bottom" do
-      end
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'a', rank: 8 }
+            b = { file: 'a', rank: 1 }
+            correct_direction = { file: 0, rank: -7 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'a', rank: 8 }
+            b = { file: 'b', rank: 1 }
+            
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from top to bottom"
 
       context "when going from bottom to top" do
-      end
-    end
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'a', rank: 1 }
+            b = { file: 'a', rank: 8 }
+            correct_direction = { file: 0, rank: 7 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'a', rank: 1 }
+            b = { file: 'b', rank: 8 }
+            
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from bottom to top"
+    end # context "when the path is vertical"
 
     context "when the path is diagonal" do
-    end
-  end
+      context "when going from top-left to bottom-right" do
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'a', rank: 8 }
+            b = { file: 'd', rank: 5 }
+            correct_direction = { file: 3, rank: -3 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'a', rank: 8 }
+            b = { file: 'd', rank: 6 }
+            
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from top-left to bottom-right"
+      
+      context "when going from bottom-right to top-left" do
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'd', rank: 5 }
+            b = { file: 'a', rank: 8 }
+            correct_direction = { file: -3, rank: 3 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'd', rank: 5 }
+            b = { file: 'a', rank: 7 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from bottom-right to top-left"
+
+      context "when going from bottom-left to top-right" do
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'a', rank: 1 }
+            b = { file: 'h', rank: 8 }
+            correct_direction = { file: 7, rank: 7 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'a', rank: 1 }
+            b = { file: 'h', rank: 7 }
+            
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from bottom-left to top-right"
+
+      context "when going from top-right to bottom-left" do
+        context "when the squares are on a straight path" do
+          it "returns the direction" do
+            a = { file: 'h', rank: 8 }
+            b = { file: 'a', rank: 1 }
+            correct_direction = { file: -7, rank: -7 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to eq(correct_direction)
+          end
+        end
+
+        context "when the squares are not on a straight path" do
+          it "returns nil" do
+            a = { file: 'h', rank: 8 }
+            b = { file: 'a', rank: 2 }
+
+            computed_direction = computer.compute_direction(a, b)
+            expect(computed_direction).to be_nil
+          end
+        end
+      end # context "when going from top-right to bottom-left"
+    end # context "when the path is diagonal"
+  end # describe '#compute_direction'
 end
