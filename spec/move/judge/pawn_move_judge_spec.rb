c0715a8f43dde587fb3499b1111eae8bdc96ab1c
Won Rhim <wwrk22@gmail.com>
2023-07-20 22:07:08 -0400

    Add PawnMoveJudge#judge_single_move unit test case - Target square is empty, but start square has the wrong chess piece.

--- spec/move/judge/pawn_move_judge_spec.rb
+++ spec/move/judge/pawn_move_judge_spec.rb
@@ -18,11 +18,23 @@ RSpec.describe PawnMoveJudge do
 
           allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
           allow(judge).to receive(:check_target).with(target_sq, board, pawn).and_return(true)
-          expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to eq(true)
+          expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to be_truthy
         end
       end
 
       context "when start square has the wrong chess piece" do
+        subject(:judge) { described_class.new }
+
+        it "returns false" do
+          target_sq = { file: 'a', rank: 3 }
+          start_sq = { file: 'a', rank: 2 }
+          pawn = { type: ChessPiece::PA, color: ChessPiece::WH }
+          board = instance_double(Board)
+
+          allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
+          allow(judge).to receive(:check_target).with(target_sq, board, pawn).and_return(false)
+          expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to be_falsey
+        end
       end
 
       context "when start square is empty" do
@@ -39,7 +51,7 @@ RSpec.describe PawnMoveJudge do
         board = instance_double(Board)
 
         allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return('a chess piece')
-        expect(judge.judge_single_move(target_sq, start_sq, pawn_color, board)).to eq(false)
+        expect(judge.judge_single_move(target_sq, start_sq, pawn_color, board)).to be_falsey
       end
     end
   end
