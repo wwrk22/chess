04bbc940edd58e612121e7b2f2e91fc093a5df75
Won Rhim <wwrk22@gmail.com>
2023-07-20 21:54:49 -0400

    Implement a case for PawnMoveJudge#judge_single_move - The case covers a single square move. - Target square is empty and start square has the right pawn.

--- lib/move/judge/pawn_move_judge.rb
+++ lib/move/judge/pawn_move_judge.rb
@@ -5,7 +5,8 @@ require './lib/move/pawn_move'
 class PawnMoveJudge < MoveJudge
   def judge_single_move(target_sq, start_sq, pawn_color, board)
     if board.at(target_sq[:file], target_sq[:rank]).nil?
-
+      pawn = { type: ChessPiece::PA, color: pawn_color }
+      return check_target(target_sq, board, pawn)
     else
       return false
     end
--- spec/move/judge/pawn_move_judge_spec.rb
+++ spec/move/judge/pawn_move_judge_spec.rb
@@ -8,7 +8,18 @@ RSpec.describe PawnMoveJudge do
   describe '#judge_single_move' do
     context "when target square is empty" do
       context "when start square has the moving pawn" do
+        subject(:judge) { described_class.new }
 
+        it "returns true" do
+          target_sq = { file: 'a', rank: 3 }
+          start_sq = { file: 'a', rank: 2 }
+          pawn = { type: ChessPiece::PA, color: ChessPiece::WH }
+          board = instance_double(Board)
+
+          allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
+          allow(judge).to receive(:check_target).with(target_sq, board, pawn).and_return(true)
+          expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to eq(true)
+        end
       end
 
       context "when start square has the wrong chess piece" do
@@ -26,6 +37,7 @@ RSpec.describe PawnMoveJudge do
         start_sq = { file: 'a', rank: 2 }
         pawn_color = ChessPiece::WH
         board = instance_double(Board)
+
         allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return('a chess piece')
         expect(judge.judge_single_move(target_sq, start_sq, pawn_color, board)).to eq(false)
       end
