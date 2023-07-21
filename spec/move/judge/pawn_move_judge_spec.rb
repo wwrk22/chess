65c9ecc785fb85961cbe9f3cb9d694c98f944f73
Won Rhim <wwrk22@gmail.com>
2023-07-21 10:57:54 -0400

    Refactor test code

--- spec/move/judge/pawn_move_judge_spec.rb
+++ spec/move/judge/pawn_move_judge_spec.rb
@@ -6,52 +6,37 @@ require './lib/standard/chess_piece'
 
 RSpec.describe PawnMoveJudge do
   describe '#judge_single_move' do
+    subject(:judge) { described_class.new }
+
+    let!(:target_sq) { { file: 'a', rank: 3 } }
+    let!(:start_sq) { { file: 'a', rank: 2 } }
+    let!(:pawn) { { type: ChessPiece::PA, color: ChessPiece::WH } }
+    let!(:board) { instance_double(Board) }
+
     context "when target square is empty" do
+      before :example do
+        allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
+      end
+
       context "when start square has the moving pawn" do
-        subject(:judge) { described_class.new }
-
         it "returns true" do
-          target_sq = { file: 'a', rank: 3 }
-          start_sq = { file: 'a', rank: 2 }
-          pawn = { type: ChessPiece::PA, color: ChessPiece::WH }
-          board = instance_double(Board)
-
-          allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
           allow(judge).to receive(:check_target).with(target_sq, board, pawn).and_return(true)
           expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to be_truthy
         end
       end
 
-      context "when start square has the wrong chess piece" do
-        subject(:judge) { described_class.new }
-
+      context "when start square does not have the moving pawn" do
         it "returns false" do
-          target_sq = { file: 'a', rank: 3 }
-          start_sq = { file: 'a', rank: 2 }
-          pawn = { type: ChessPiece::PA, color: ChessPiece::WH }
-          board = instance_double(Board)
-
-          allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
           allow(judge).to receive(:check_target).with(target_sq, board, pawn).and_return(false)
           expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to be_falsey
         end
       end
-
-      context "when start square is empty" do
-      end
     end # context "when target square is empty"
 
     context "when target square is not empty" do
-      subject(:judge) { described_class.new }
-
       it "returns false" do
-        target_sq = { file: 'a', rank: 3 }
-        start_sq = { file: 'a', rank: 2 }
-        pawn_color = ChessPiece::WH
-        board = instance_double(Board)
-
         allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return('a chess piece')
-        expect(judge.judge_single_move(target_sq, start_sq, pawn_color, board)).to be_falsey
+        expect(judge.judge_single_move(target_sq, start_sq, pawn[:color], board)).to be_falsey
       end
     end # context "when target square is not empty"
   end # describe '#judge_single_move'
