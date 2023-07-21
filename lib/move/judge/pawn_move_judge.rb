c6d7b96120b2898418f82e20e486cdaa9b7d9513
Won Rhim <wwrk22@gmail.com>
2023-07-21 13:56:51 -0400

    Refactor to reduce code, and add method description comments

--- lib/move/judge/pawn_move_judge.rb
+++ lib/move/judge/pawn_move_judge.rb
@@ -26,26 +26,30 @@ class PawnMoveJudge < MoveJudge
     end
   end
 
+  ##
+  # Return false if the target square is empty or has the player's own chess
+  # piece. Otherwise, determine the start square with `start_file` and return
+  # true if the square has the player's pawn, or false if it doesn't.
   def judge_capture(target_sq, start_file, pawn_color, board)
     target = board.at(target_sq[:file], target_sq[:rank])
+    return false if target.nil? || target[:color] == pawn_color
 
-    if target.nil?
-      return false
-    else
-      if target[:color] == pawn_color
-        return false
-      else # target chess piece is opponent's
-        start_rank = pawn_color == ChessPiece::WH ? target_sq[:rank] - 1 : target_sq[:rank] + 1
-        start_sq = { file: start_file, rank: start_rank }
-
-        pawn = { type: ChessPiece::PA, color: pawn_color }
-        return check_target(start_sq, board, pawn)
-      end
-    end
+    start_rank = compute_start_rank(target_sq[:rank], pawn_color)
+    start_sq = { file: start_file, rank: start_rank }
+    check_target(start_sq, board, { type: ChessPiece::PA, color: pawn_color })
   end
 
   private
 
+  ##
+  # Simply determine the rank of the starting square of a capturing pawn.
+  def compute_start_rank(target_rank, pawn_color)
+    return pawn_color == ChessPiece::WH ? target_rank - 1 : target_rank + 1
+  end
+
+  ##
+  # Determine whether the square "before" the target square is empty depending
+  # on the player's color.
   def mid_sq_empty?(target_sq, pawn_color, board)
     if pawn_color == ChessPiece::WH
       return board.at(target_sq[:file], target_sq[:rank] - 1).nil?
