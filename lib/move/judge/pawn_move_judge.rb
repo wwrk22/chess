6b994909510f5345e5459adcf2cf062890a4d6a6
Won Rhim <wwrk22@gmail.com>
2023-07-21 11:03:16 -0400

    Refactor to reduce code

--- lib/move/judge/pawn_move_judge.rb
+++ lib/move/judge/pawn_move_judge.rb
@@ -4,11 +4,10 @@ require './lib/move/pawn_move'
 
 class PawnMoveJudge < MoveJudge
   def judge_single_move(target_sq, start_sq, pawn_color, board)
-    if board.at(target_sq[:file], target_sq[:rank]).nil?
-      pawn = { type: ChessPiece::PA, color: pawn_color }
-      return check_target(target_sq, board, pawn)
-    else
-      return false
-    end
+    file, rank = [target_sq[:file], target_sq[:rank]]
+    pawn = { type: ChessPiece::PA, color: pawn_color }
+
+    return check_target(target_sq, board, pawn) if board.at(file, rank).nil?
+    return false # if target square is not empty
   end
 end
