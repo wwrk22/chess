2bb2710d6971334c0ae1298fbb44e5d4beac7999
Won Rhim <wwrk22@gmail.com>
2023-07-21 16:11:31 -0400

    Implement another case for RookMoveJudge#judge_move - Case is where the target square is empty but the start square does not have the player's rook.

--- lib/move/judge/rook_move_judge.rb
+++ lib/move/judge/rook_move_judge.rb
@@ -6,6 +6,13 @@ require './lib/board/board'
 class RookMoveJudge < MoveJudge
   def judge_move(start_sq, target_sq, player_color, board)
     if board.at(target_sq[:file], target_sq[:rank]).nil?
+      player_rook = { type: ChessPiece::RO, color: player_color }
+
+      if check_target(start_sq, board, player_rook).nil?
+        return false
+      else
+
+      end
     else
       return false
     end
--- spec/move/judge/rook_move_judge_spec.rb
+++ spec/move/judge/rook_move_judge_spec.rb
@@ -6,21 +6,31 @@ require './lib/board/board'
 RSpec.describe RookMoveJudge do
   describe '#judge_move' do
     subject(:judge) { described_class.new }
+    
+    let!(:start_sq) { { file: 'a', rank: 1 } }
+    let!(:target_sq) { { file: 'a', rank: 5 } }
+    let!(:player_color) { ChessPiece::WH }
+    let!(:board) { instance_double(Board) }
 
     context "when the target square is not empty" do
       it "returns false" do
-        start_sq = { file: 'a', rank: 1 }
-        target_sq = { file: 'a', rank: 5 }
-        player_color = ChessPiece::WH
-        board = instance_double(Board)
-
         allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return('some chess piece')
-
         expect(judge.judge_move(start_sq, target_sq, player_color, board)).to be_falsey
       end
     end
 
     context "when the target square is empty" do
+      context "when the start square does not have the player's rook" do
+        it "returns false" do
+          player_rook = { type: ChessPiece::RO, color: player_color }
+          allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
+          allow(judge).to receive(:check_target).with(start_sq, board, player_rook).and_return(false)
+          expect(judge.judge_move(start_sq, target_sq, player_color, board)).to be_falsey
+        end
+      end
+
+      context "when the start square has the player's rook" do
+      end
     end
   end
 end
