c24d230bdf4e78288933a9fbe1115c77bca3350d
Won Rhim <wwrk22@gmail.com>
2023-07-20 20:57:54 -0400

    Judge move legality when pawn has two possible starts for its first move - Added unit tests with basic cases.

--- lib/move/judge/pawn_move_judge.rb
+++ lib/move/judge/pawn_move_judge.rb
@@ -13,6 +13,18 @@ class PawnMoveJudge < MoveJudge
         start_has_pawn = check_target(move.starts[0], board, move.moving_piece)
         return target_empty && start_has_pawn
       end
+
+      if move.starts.size == 2
+        target_empty = check_target(move.target, board)
+        first_start_has_pawn = check_target(move.starts[0], board, move.moving_piece)
+
+        return true if target_empty && first_start_has_pawn
+
+        second_start_has_pawn = check_target(move.starts[1], board, move.moving_piece)
+        first_start_empty = check_target(move.starts[0], board)
+
+        return true if target_empty && first_start_empty && second_start_has_pawn
+      end
     end
   end
 end
--- spec/move/judge/pawn_move_judge_spec.rb
+++ spec/move/judge/pawn_move_judge_spec.rb
@@ -1,31 +1,68 @@
 require './lib/move/judge/pawn_move_judge'
 require './lib/board/board'
 require './lib/move/pawn_move'
+require './lib/standard/chess_piece'
 
 
 RSpec.describe PawnMoveJudge do
   describe '#judge' do
     context "when the move is not a capture" do
       context "when there is one start square" do
-        subject(:judge) { described_class.new }
+        context "when the right pieces are in place" do
+          subject(:judge) { described_class.new }
 
-        it "calls #check_target on the start and target squares" do
-          move = PawnMove.new
-          move.target = { file: 'a', rank: 3 }
-          move.starts = [{ file: 'a', rank: 2 }]
-          move.capture = false
-          board = instance_double(Board)
-          allow(judge).to receive(:check_target).with(move.target, board).and_return(true)
-          allow(judge).to receive(:check_target).with(move.starts[0], board, move.moving_piece).and_return(true)
-          expect(judge.judge_move(move, board)).to eq(true)
+          it "returns true" do
+            move = PawnMove.new
+            move.target = { file: 'a', rank: 3 }
+            move.starts = [{ file: 'a', rank: 2 }]
+            move.capture = false
+            move.moving_piece = { type: ChessPiece::PA, color: ChessPiece::WH }
+            board = instance_double(Board)
+
+            allow(judge).to receive(:check_target).with(move.target, board).and_return(true)
+            allow(judge).to receive(:check_target).with(move.starts[0], board, move.moving_piece).and_return(true)
+            expect(judge.judge_move(move, board)).to eq(true)
+          end
         end
-      end
+      end # context "when there is one start square"
 
       context "when there are two start squares" do
-        subject(:judge) { described_class.new }
+        context "when the start closer to target has the moving pawn" do
+          subject(:judge) { described_class.new }
+          
+          it "returns true" do
+            move = PawnMove.new
+            move.target = { file: 'a', rank: 4 }
+            move.starts = [{ file: 'a', rank: 3 }, { file: 'a', rank: 2 }]
+            move.capture = false
+            move.moving_piece = { type: ChessPiece::PA, color: ChessPiece::WH }
+            board = instance_double(Board)
 
-        
-      end
+            allow(judge).to receive(:check_target).with(move.target, board).and_return(true)
+            allow(judge).to receive(:check_target).with(move.starts[0], board, move.moving_piece).and_return(true)
+            expect(judge.judge_move(move, board)).to eq(true)
+          end
+        end
+
+        context "when the start further away from the target has the moving pawn" do
+          subject(:judge) { described_class.new }
+          
+          it "returns true" do
+            move = PawnMove.new
+            move.target = { file: 'a', rank: 4 }
+            move.starts = [{ file: 'a', rank: 3 }, { file: 'a', rank: 2 }]
+            move.capture = false
+            move.moving_piece = { type: ChessPiece::PA, color: ChessPiece::WH }
+            board = instance_double(Board)
+
+            allow(judge).to receive(:check_target).with(move.target, board).and_return(true)
+            allow(judge).to receive(:check_target).with(move.starts[0], board, move.moving_piece).and_return(false)
+            allow(judge).to receive(:check_target).with(move.starts[1], board, move.moving_piece).and_return(true)
+            allow(judge).to receive(:check_target).with(move.starts[0], board).and_return(true)
+            expect(judge.judge_move(move, board)).to eq(true)
+          end
+        end
+      end # context "when there are two start squares"
     end # context "when the move is not a capture"
 
     context "when the move is a capture" do
