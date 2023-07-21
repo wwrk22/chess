98b410de899cc1838ae662aed54935cda78805de
Won Rhim <wwrk22@gmail.com>
2023-07-21 19:01:07 -0400

    In RookMoveJudge#judge_move, check the path between empty target square and moving rook - Added unit tests for remaining cases.

--- lib/move/judge/rook_move_judge.rb
+++ lib/move/judge/rook_move_judge.rb
@@ -1,20 +1,22 @@
 require_relative './move_judge'
 require './lib/standard/chess_piece'
 require './lib/board/board'
+require './lib/board/board_computer'
 
 
 class RookMoveJudge < MoveJudge
+  include BoardComputer
+
   def judge_move(start_sq, target_sq, player_color, board)
     if board.at(target_sq[:file], target_sq[:rank]).nil?
       player_rook = { type: ChessPiece::RO, color: player_color }
 
-      if check_target(start_sq, board, player_rook).nil?
-        return false
-      else
-
+      if check_target(start_sq, board, player_rook)
+        direction = compute_direction(start_sq, target_sq)
+        return clear_path?(start_sq, target_sq, board, direction)
       end
-    else
-      return false
     end
+
+    return false
   end
 end
--- spec/move/judge/rook_move_judge_spec.rb
+++ spec/move/judge/rook_move_judge_spec.rb
@@ -15,22 +15,36 @@ RSpec.describe RookMoveJudge do
     context "when the target square is not empty" do
       it "returns false" do
         allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return('some chess piece')
+
         expect(judge.judge_move(start_sq, target_sq, player_color, board)).to be_falsey
       end
-    end
+    end # context "when the target square is not empty"
 
     context "when the target square is empty" do
+      let!(:player_rook) { { type: ChessPiece::RO, color: player_color } }
+
+      before :example do
+        allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
+      end
+
       context "when the start square does not have the player's rook" do
         it "returns false" do
-          player_rook = { type: ChessPiece::RO, color: player_color }
-          allow(board).to receive(:at).with(target_sq[:file], target_sq[:rank]).and_return(nil)
           allow(judge).to receive(:check_target).with(start_sq, board, player_rook).and_return(false)
+
           expect(judge.judge_move(start_sq, target_sq, player_color, board)).to be_falsey
         end
       end
 
       context "when the start square has the player's rook" do
-      end
-    end
-  end
+        it "calls #clear_path? to check if the path between start and target is clear" do
+          direction = { file: 0, rank: 4 }
+          allow(judge).to receive(:check_target).with(start_sq, board, player_rook).and_return(true)
+          allow(judge).to receive(:compute_direction).with(start_sq, target_sq).and_return(direction)
+
+          expect(judge).to receive(:clear_path?).with(start_sq, target_sq, board, direction)
+          judge.judge_move(start_sq, target_sq, player_color, board)
+        end
+      end # context "when the start square has the player's rook"
+    end # context "when the target square is empty"
+  end # describe '#judge_move'
 end
