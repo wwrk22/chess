29590109cf3dafe9d71be5bc4858af15774e5486
Won Rhim <wwrk22@gmail.com>
2023-07-24 11:07:14 -0400

    Remove: MoveJudge#check_target method and its unit tests

--- lib/move/judge/move_judge.rb
+++ lib/move/judge/move_judge.rb
@@ -12,15 +12,6 @@ class MoveJudge
     return target.nil? ? false : target[:color] == color
   end
 
-  def check_target(target_square, board, target = nil)
-    target_file = target_square[:file]
-    target_rank = target_square[:rank]
-    actual_target = board.at(target_file, target_rank)
-
-    return actual_target.nil? if target.nil?
-    return (actual_target.nil?) ? false : (actual_target == target)
-  end
-
   # Check to see if the path between the starting square 'a', and the ending
   # square 'b' is clear. The 'direction' tells which way to go from 'a'.
   def clear_path?(a, b, board, direction)
