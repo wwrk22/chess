63d9d5c9e6cd7e64cd281639bf5f8f0f44a189b0
Won Rhim <wwrk22@gmail.com>
2023-07-27 11:13:32 -0400

    Add a module to store constants that describe properties of chess pieces - Added attribute to Pawn to tell its color so it knows which unicode string to use.

--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -1,10 +1,23 @@
 require_relative './chess_piece'
+require_relative './piece_specs'
 
 
 class Pawn < ChessPiece
+  include PieceSpecs
+
   UNICODE_WH = "\u2659"
   UNICODE_BL = "\u265F"
   private_constant :UNICODE_WH, :UNICODE_BL
+
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+
+  def unicode
+    return @color == white ? UNICODE_WH : UNICODE_BL
+  end
 
   class << self
     def unicode_wh
---
+++ lib/piece/piece_specs.rb
@@ -0,0 +1,13 @@
+module PieceSpecs
+  WHITE = 'WH'
+  BLACK = 'BL'
+  private_constant :WHITE, :BLACK
+
+  def white
+    WHITE
+  end
+
+  def black
+    BLACK
+  end
+end
