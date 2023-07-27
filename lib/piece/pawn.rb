503cf1b2a1afb5da423422fa9d8c602540e2c29e
Won Rhim <wwrk22@gmail.com>
2023-07-27 10:53:42 -0400

    Add class methods to make Pawn class unicode strings publicly available

--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -5,4 +5,14 @@ class Pawn < ChessPiece
   UNICODE_WH = "\u2659"
   UNICODE_BL = "\u265F"
   private_constant :UNICODE_WH, :UNICODE_BL
+
+  class << self
+    def unicode_wh
+      UNICODE_WH
+    end
+
+    def unicode_bl
+      UNICODE_BL
+    end
+  end
 end
