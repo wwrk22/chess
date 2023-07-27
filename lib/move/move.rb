f7bed46d3c53bb1f80a8ddc20a57324923d48749
Won Rhim <wwrk22@gmail.com>
2023-07-27 16:39:42 -0400

    Clean up

--- lib/move/move.rb
+++ lib/move/move.rb
@@ -14,19 +14,14 @@ class Move
   # The kind of chess piece being moved.
   attr_accessor :piece
 
+  # The possible starting squares of the moving piece used only when the moving
+  # piece is a pawn or a knight.
+  attr_accessor :start
+
   # The target square that the @piece is moving to or capturing.
   attr_accessor :target
 
   # Indicate whether or not the move is a capture.
   # Format: Boolean
   attr_accessor :capture
-
-  # The possible starting squares of the moving piece used only when the moving
-  # piece is a pawn or a knight.
-  attr_accessor :start
-
-  # The directions from @target that can be followed to check for possible
-  # starting squares of @piece. This is used by all piece types except
-  # for pawn and knight.
-  attr_accessor :directions
 end
