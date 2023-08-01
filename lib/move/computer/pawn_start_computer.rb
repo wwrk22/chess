92339af857ae561964567dca690d69847705954f
Won Rhim <wwrk22@gmail.com>
2023-08-01 19:38:03 -0400

    Add: PawnStartComputer#compute_move

--- lib/move/computer/pawn_start_computer.rb
+++ lib/move/computer/pawn_start_computer.rb
@@ -19,17 +19,29 @@ class PawnStartComputer < StartComputer
     start_square if check_start(start_square, move.piece, board)
   end
 
-  ##
-  # Return the starting square of the moving pawn for a non-capturing, single
-  # square move.
-  def compute_single(move, board)
+  def compute_move(move, board)
+    color, target_file, target_rank = [move.piece.color, move.target[:file], move.target[:rank]]
     rank_diff = (move.piece.color == white) ? -1 : 1
-    target_file, target_rank = [move.target[:file], move.target[:rank]]
-    pawn = board.at(target_file, target_rank + rank_diff)
     
-    { file: target_file, rank: target_rank + rank_diff } if pawn.eql? move.piece
+    first_start = compute_start_square(rank_diff, move, board)
+    return first_start if first_start
+
+    first_start_empty = board.at(target_file, target_rank + rank_diff).nil?
+    white_double = (color == white) && (target_rank == 4)
+    black_double = (color == black) && (target_rank == 5)
+
+    if first_start_empty && (white_double || black_double)
+      second_start = compute_start_square(rank_diff * 2, move, board)
+      return second_start if second_start
+    end
   end
 
-  def compute_double(move, board)
+  private
+
+  def compute_start_square(rank_diff, move, board)
+    target_file, target_rank = [move.target[:file], move.target[:rank]]
+    pawn = board.at(target_file, target_rank + rank_diff)
+
+    { file: target_file, rank: target_rank + rank_diff } if pawn.eql? move.piece
   end
 end
