08a43bd523e948e067377ed333ca26b5936a1538
Won Rhim <wwrk22@gmail.com>
2023-08-01 11:41:25 -0400

    Add: PawnStartComputer#compute_single - Compute the starting square for a pawn moving one square. - Added unit tests.

--- spec/move/computer/pawn_start_computer.rb
+++ spec/move/computer/pawn_start_computer.rb
@@ -40,6 +40,7 @@ RSpec.describe PawnStartComputer do
     end # context "when pawn is black"
   end # describe '#compute_capture'
 
+
   describe '#compute_capture' do
     subject(:computer) { described_class.new }
 
@@ -47,19 +48,56 @@ RSpec.describe PawnStartComputer do
       move = instance_double(Move)
       board = instance_double(Board)
       white_pawn = instance_double(ChessPiece)
+      expected_start = { file: 'b', rank: 2 }
 
       allow(white_pawn).to receive(:type).and_return(pawn)
       allow(white_pawn).to receive(:color).and_return(white)
 
       allow(move).to receive(:piece).and_return(white_pawn)
-      allow(move).to receive(:color).and_return(white)
       allow(move).to receive(:start_coordinate).and_return('b')
       allow(move).to receive(:target).and_return({ file: 'a', rank: 3 })
 
-      allow(board).to receive(:at).with('b', 2).and_return(white_pawn)
+      allow(board).to receive(:at).with(expected_start[:file], expected_start[:rank]).and_return(white_pawn)
 
       result = computer.compute_capture(move, board)
-      expect(result).to be_truthy
+      expect(result).to eq(expected_start)
     end
   end # describe '#compute_capture'
+
+
+  describe '#compute_single' do
+    subject(:computer) { described_class.new }
+
+    context "when the move is for white" do
+      it "returns the square directly below the target square" do
+        move = instance_double(Move)
+        target_square = { file: 'a', rank: 3 }
+        white_pawn = instance_double(ChessPiece)
+
+        allow(white_pawn).to receive(:color).and_return(white)
+        allow(move).to receive(:target).and_return(target_square)
+        allow(move).to receive(:piece).and_return(white_pawn)
+
+        expected = { file: 'a', rank: 2 }
+        result = computer.compute_single(move)
+        expect(result).to eq(expected)
+      end
+    end
+
+    context "when the move is for black" do
+      it "returns the square directly below the target square" do
+        move = instance_double(Move)
+        target_square = { file: 'a', rank: 6 }
+        black_pawn = instance_double(ChessPiece)
+
+        allow(black_pawn).to receive(:color).and_return(black)
+        allow(move).to receive(:target).and_return(target_square)
+        allow(move).to receive(:piece).and_return(black_pawn)
+
+        expected = { file: 'a', rank: 7 }
+        result = computer.compute_single(move)
+        expect(result).to eq(expected)
+      end
+    end
+  end # describe '#compute_move'
 end
