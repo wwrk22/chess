f4be11b295f42857a34d490538a6639ecdad0a3d
Won Rhim <wwrk22@gmail.com>
2023-08-02 16:18:54 -0400

    Add: StartComputer#check_path - Compute the starting square of a moving piece if it's on a clear path from the target. - Added unit tests.

--- lib/move/computer/start_computer.rb
+++ lib/move/computer/start_computer.rb
@@ -11,6 +11,14 @@ class StartComputer
       false : target.type == piece.type && target.color == piece.color
   end
 
+  ##
+  # Take a moving piece, the target square, and a direction to determine
+  # whether or not the piece is on the path in the direction from the target
+  # square.
+  def check_path(move, board, direction)
+    curr_square = update_square(move.target, direction)
+    check_each_square(curr_square, move, board, direction)
+  end
 
   ##
   # [Abstract]
@@ -18,4 +26,23 @@ class StartComputer
   # squares must be empty between the moving chess piece and the target square.
   def calculate_limit; raise "SubclassResponsibility"; end
 
+
+  private
+
+  def check_each_square(curr_square, move, board, direction)
+    while valid_file?(curr_square[:file]) && valid_rank?(curr_square[:rank]) do
+      piece = board.at(curr_square[:file], curr_square[:rank])
+      return check_piece(piece, move.piece, curr_square) if piece
+      curr_square = update_square(curr_square, direction)
+    end
+  end
+
+  def check_piece(piece, moving_piece, curr_square)
+    return curr_square if piece.eql? moving_piece
+  end
+
+  def update_square(square, direction)
+    file_ord = square[:file].ord + direction[:file]
+    { file: file_ord.chr, rank: square[:rank] + direction[:rank] }
+  end
 end
--- spec/move/computer/start_computer_spec.rb
+++ spec/move/computer/start_computer_spec.rb
@@ -2,6 +2,7 @@ require './lib/move/computer/start_computer'
 require './lib/piece/piece_specs'
 require './lib/board/board'
 require './lib/piece/chess_piece'
+require './lib/move/move'
 
 
 RSpec.configure do |cfg|
@@ -26,4 +27,49 @@ RSpec.describe StartComputer do
       expect(result).to be_truthy
     end
   end # describe '#check_start'
+
+
+  describe '#check_path' do
+    subject(:computer) { described_class.new }
+
+    let(:move) { instance_double(Move) }
+    let(:board) { instance_double(Board) }
+    let(:white_rook) { ChessPiece.new(rook, white) }
+    let(:direction) { { file: 0, rank: -1 } }
+
+    before do
+      allow(move).to receive(:target).and_return({ file: 'a', rank: 3 })
+      allow(move).to receive(:piece).and_return(white_rook)
+    end
+
+    context "when the piece is on the path and can get to the target square" do
+      it "returns the square that the piece is on" do
+        allow(board).to receive(:at).with('a', 2).and_return nil
+        allow(board).to receive(:at).with('a', 1).and_return white_rook
+
+        result = computer.check_path(move, board, direction)
+        expected = { file: 'a', rank: 1 }
+        expect(result).to eq(expected)
+      end
+    end
+
+    context "when the piece is on the path but cannot get to the target square" do
+      it "returns nil" do
+        allow(board).to receive(:at).with('a', 2).and_return ChessPiece.new(pawn, white)
+        
+        result = computer.check_path(move, board, direction)
+        expect(result).to be_nil
+      end
+    end
+
+    context "when the piece is not on the path" do
+      it "returns nil" do
+        allow(board).to receive(:at).with('a', 2).and_return nil
+        allow(board).to receive(:at).with('a', 1).and_return nil
+        
+        result = computer.check_path(move, board, direction)
+        expect(result).to be_nil
+      end
+    end
+  end # describe '#check_path'
 end
