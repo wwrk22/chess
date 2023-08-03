edc788cd14dc76d2d0966c5c8fe80853ad0e73d7
Won Rhim <wwrk22@gmail.com>
2023-08-03 11:16:27 -0400

    Remove: obsolete method

--- lib/move/computer/rook_start_computer.rb
+++ lib/move/computer/rook_start_computer.rb
@@ -6,16 +6,6 @@ require './lib/piece/rook_specs'
 class RookStartComputer < StartComputer
   include BoardSpecs
 
-  # Compute the starting square only if a valid starting file or rank is given.
-  def compute_move_old(target_square, start_place)
-    start_file, start_rank = [target_square[:file], target_square[:rank]]
-    start_file = start_place if valid_file? start_place
-    start_rank = start_place if valid_rank? start_place
-
-    start = { file: start_file, rank: start_rank }
-    (start == target) ? nil : start
-  end
-
   ##
   # Accept a Move object and a copy of the Board to return the starting square
   # of a rook. If the starting square cannot be computed, return nil.
