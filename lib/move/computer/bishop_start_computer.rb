a777d4e68afab3ad564b803be9f7a3d6655bbea7
Won Rhim <wwrk22@gmail.com>
2023-08-03 14:37:25 -0400

    Add: BishopStartComputer#compute_with_file - Compute the possible startin squares when the starting file is given.

--- lib/move/computer/bishop_start_computer.rb
+++ lib/move/computer/bishop_start_computer.rb
@@ -15,4 +15,35 @@ class BishopStartComputer
       return compute_with_start_place(move.target, start_place)
     end
   end
+
+  ##
+  # Compute the starting square of a bishop with the given starting coordinate.
+  # Return the square if the bishop is on the square. Otherwise, return nil.
+  def compute_with_start_coordinate(target_square, start_coordinate, board, moving_bishop)
+    # Find the two possible starting squares.
+    # Return the one square that has the moving bishop.
+    # Return nil if neither has the moving bishop.
+    #starting_square = foo()
+
+    #starting_square(proc {}) { |square| board.at(square[:file], square[:rank]).
+  end
+
+  def compute_with_file(target_square, start_file)
+    starting_ranks = compute_start_ranks(target_square, start_file)
+
+    starting_ranks.map do |rank|
+      { file: start_file, rank: rank } if valid_rank? rank
+    end
+  end
+
+  def compute_with_rank(target_square, start_coordinate)
+  end
+
+  
+  private
+  
+  def compute_start_ranks(target_square, start_file)
+    rank_diff = (target_square[:file].ord - start_file.ord).abs
+    [target_square[:rank] - rank_diff, target_square[:rank] + rank_diff]
+  end
 end
--- spec/move/computer/bishop_start_computer.rb
+++ spec/move/computer/bishop_start_computer.rb
@@ -1,7 +1,13 @@
 require './lib/move/computer/bishop_start_computer'
 require './lib/move/move'
 require './lib/board/board'
+require './lib/piece/chess_piece'
+require './lib/piece/piece_specs'
 
+
+RSpec.configure do |cfg|
+  cfg.include PieceSpecs
+end
 
 RSpec.describe BishopStartComputer do
   describe '#compute_move' do
@@ -16,4 +22,37 @@ RSpec.describe BishopStartComputer do
       computer.compute_move(move, board)
     end
   end # describe '#compute_move'
+
+  
+  describe '#compute_with_file' do
+    subject(:computer) { described_class.new }
+
+    context "when two starting squares are possible" do
+      it "returns the two squares on the starting file that the bishop can move from" do
+        target_square = { file: 'e', rank: 3 }
+        start_file = 'c'
+        expected = [{ file: start_file, rank: 1 }, { file: start_file, rank: 5 }]
+
+        starting_squares = computer.compute_with_file(target_square, start_file)
+
+        result = expected.difference(starting_squares).none? &&
+                 starting_squares.difference(expected).none?
+        expect(result).to be_truthy
+      end
+    end
+
+    context "when only one starting square is possible" do
+      it "returns the one square on the starting file that the bishop can move from" do
+        target_square = { file: 'c', rank: 1 }
+        start_file = 'b'
+        expected = [{ file: start_file, rank: 2 }]
+
+        starting_square = computer.compute_with_file(target_square, start_file)
+
+        result = expected.difference(starting_square).none? &&
+                 starting_square.difference(expected).none?
+        expect(result).to be_truthy
+      end
+    end
+  end # describe '#compute_with_file'
 end
