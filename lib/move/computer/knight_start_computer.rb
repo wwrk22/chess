d327df770675580e990a679093d81abf322a603a
Won Rhim <wwrk22@gmail.com>
2023-08-05 13:08:32 -0400

    feat: compute the actual starting square of a knight

---
+++ lib/move/computer/knight_start_computer.rb
@@ -0,0 +1,44 @@
+require './lib/board/board_specs'
+require './lib/piece/knight_specs'
+
+
+# Computes the required information for use by the chess board in order to
+# perform a move with a knight.
+class KnightStartComputer
+  include BoardSpecs
+  include KnightSpecs
+  
+  def compute_move(move, board)
+    starts = all_possible_starts(move.target)
+    find_knight(move.piece, board, starts)
+  end
+
+  def find_knight(knight, board, starts)
+    starts.filter! { |start| board.at(start).eql? knight }
+    starts[0] if starts.size == 1
+  end
+
+  def all_possible_starts(target_sq)
+    starts = top_half_starts(target_sq[:file], target_sq[:rank]) +
+      bottom_half_starts(target_sq[:file], target_sq[:rank])
+
+    starts.filter { |start| valid_square?(start) }
+  end
+
+
+  private
+
+  def top_half_starts(file, rank)
+    [{ file: (file.ord + dir[0][:file]).chr, rank: rank + dir[0][:rank] },
+     { file: (file.ord + dir[1][:file]).chr, rank: rank + dir[1][:rank] },
+     { file: (file.ord + dir[2][:file]).chr, rank: rank + dir[2][:rank] },
+     { file: (file.ord + dir[3][:file]).chr, rank: rank + dir[3][:rank] }]
+  end
+
+  def bottom_half_starts(file, rank)
+    [{ file: (file.ord + dir[4][:file]).chr, rank: rank + dir[4][:rank] },
+     { file: (file.ord + dir[5][:file]).chr, rank: rank + dir[5][:rank] },
+     { file: (file.ord + dir[6][:file]).chr, rank: rank + dir[6][:rank] },
+     { file: (file.ord + dir[7][:file]).chr, rank: rank + dir[7][:rank] }]
+  end
+end
---
+++ spec/move/computer/knight_start_computer.rb
@@ -0,0 +1,51 @@
+require './lib/board/board'
+require './lib/move/computer/knight_start_computer'
+require './lib/piece/chess_piece'
+require './lib/piece/piece_specs'
+
+
+RSpec.configure do |cfg|
+  cfg.include PieceSpecs
+end
+
+RSpec.describe KnightStartComputer do
+  describe '#compute_move' do
+
+    let!(:move) { instance_double(Move) }
+    let!(:board) { instance_double(Board) }
+
+  end # describe '#compute_move'
+
+
+  describe '#all_possible_starts' do
+    subject(:computer) { described_class.new }
+
+    it "filters out starts that have invalid coordinates" do
+      target_sq = { file: 'a', rank: 1 }
+      expected = [{ file: 'b', rank: 3 }, { file: 'c', rank: 2 }]
+
+      filtered_starts = computer.all_possible_starts(target_sq)
+
+      expect(filtered_starts).to eq(expected)
+    end
+  end # describe '#all_possible_starts'
+
+
+  describe '#find_knight' do
+    subject(:computer) { described_class.new }
+
+    it "returns a starting square if there is only one such that the knight is on" do
+      white_knight = ChessPiece.new(knight, white)
+      empty_sq = { file: 'c', rank: 3 }
+      expected_sq = { file: 'd', rank: 2 }
+      starts = [empty_sq, expected_sq]
+      board = instance_double(Board)
+
+      allow(board).to receive(:at).with(starts[0]).and_return nil
+      allow(board).to receive(:at).with(starts[1]).and_return white_knight
+
+      result = computer.find_knight(white_knight, board, starts)
+      expect(result).to eq(expected_sq)
+    end
+  end # describe '#find_knight'
+end
