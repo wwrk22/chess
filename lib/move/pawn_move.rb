5ad6c1d5e25fbf40cb63ac0ced9174f9f5032f9a
Won Rhim <wwrk22@gmail.com>
2023-08-05 16:05:27 -0400

    chore: use shorter naming

--- lib/move/pawn_move.rb
+++ lib/move/pawn_move.rb
@@ -4,8 +4,8 @@ require_relative './move'
 class PawnMove < Move
   
   # Indicate whether or not the capture is an en passant.
-  attr_accessor :en_passant
+  attr_accessor :ep
 
   # The square of the opponent's pawn during an en passant.
-  attr_accessor :en_passant_square
+  attr_accessor :ep_sq
 end
