715d3664aac646ce8b835e40674bb4594cf16668
Won Rhim <wwrk22@gmail.com>
2023-08-05 17:03:41 -0400

    chore: clean up

--- lib/move/performer/move_performer.rb
+++ lib/move/performer/move_performer.rb
@@ -4,8 +4,7 @@ class MovePerformer
   # Perform the move on the board. Return true if the move was performed, or
   # false if it could not be performed.
   def do_move(move, board)
-    return false if board.at_sq(move.target).nil? == false
-
+    return false if not board.at_sq(move.target).nil?
     board.set(move.start)
     board.set(move.target, move.piece)
     true
