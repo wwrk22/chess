b330b5174cd88add1e95a1b3740ab4ac2c7a1d69
Won Rhim <wwrk22@gmail.com>
2023-08-05 16:04:17 -0400

    chore: clean up comments

--- lib/move/move.rb
+++ lib/move/move.rb
@@ -8,20 +8,20 @@ class Move
   # The raw string format of the move given by player.
   attr_accessor :str
 
-  # The kind of chess piece being moved.
+  # The exact chess piece being moved.
   attr_accessor :piece
 
   # The file or rank the moving piece is at.
   attr_accessor :start_coordinate
 
-  # The possible starting squares of the moving piece used only when the moving
-  # piece is a pawn or a knight.
+  # The exact square from which the moving piece starts. If after a move start
+  # computation this value is nil, then it indicates this Move as a whole is
+  # illegal.
   attr_accessor :start
 
-  # The target square that the @piece is moving to or capturing.
+  # The target square that the moving piece is moving to or capturing at.
   attr_accessor :target
 
   # Indicate whether or not the move is a capture.
-  # Format: Boolean
   attr_accessor :capture
 end
