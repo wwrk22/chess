dc1a715d28811817cbbf5aa08c4b105037a07985
Won Rhim <wwrk22@gmail.com>
2023-08-05 16:20:39 -0400

    chore: simplify code

--- lib/move/syntax/validator/bishop_validator.rb
+++ lib/move/syntax/validator/bishop_validator.rb
@@ -10,7 +10,7 @@ class BishopValidator
   # Return the move if move has valid syntax. Otherwise, return nil.
   # Raise ColorUnknownError if color is unknown.
   def validate(move_str, color)
-    raise ColorUnknownError.new(color) if valid_color?(color) == false
-    (move_str =~ BishopMoves::MOVE) ? ChessPiece.new(bishop, color) : nil
+    raise ColorUnknownError.new(color) if not valid_color?(color)
+    ChessPiece.new(bishop, color) if (move_str =~ BishopMoves::MOVE)
   end
 end
