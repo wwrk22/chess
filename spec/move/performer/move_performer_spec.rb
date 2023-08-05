ddc0f3c8e1ec726756e215688201d1fb5e1c7630
Won Rhim <wwrk22@gmail.com>
2023-08-05 15:50:57 -0400

    feat: MovePerformer can perform a capturing move - Included unit tests.

--- spec/move/performer/move_performer_spec.rb
+++ spec/move/performer/move_performer_spec.rb
@@ -9,16 +9,19 @@ RSpec.configure do |cfg|
   cfg.include PieceSpecs
 end
 
-RSpec.describe MovePerformer do
+RSpec.describe MovePerformer do  
+  let!(:move) { instance_double(Move) }
+  let!(:board) { instance_double(Board) }
+
+  before do
+    allow(move).to receive(:piece).and_return(ChessPiece.new(pawn, white))
+  end
+
   describe '#do_move' do
     subject(:performer) { described_class.new }
-    
-    let!(:move) { instance_double(Move) }
-    let!(:board) { instance_double(Board) }
 
     before do
       allow(move).to receive(:target).and_return({ file: 'a', rank: 3 })
-      allow(move).to receive(:piece).and_return(ChessPiece.new(pawn, white))
       allow(move).to receive(:start).and_return({ file: 'a', rank: 2})
     end
 
@@ -42,4 +45,32 @@ RSpec.describe MovePerformer do
       end
     end
   end # describe '#do_move'
+
+
+  describe '#do_capture' do
+    subject(:computer) { described_class.new }
+
+    before do
+      allow(move).to receive(:target).and_return({ file: 'b', rank: 3 })
+      allow(move).to receive(:start).and_return({ file: 'a', rank: 2 })
+    end
+
+    context "when the target square has an opponent piece" do
+      it "sends do_move and returns the result" do
+        allow(board).to receive(:at_sq).with(move.target).and_return ChessPiece.new(pawn, black)
+
+        expect(computer).to receive(:do_move).with(move, board)
+        computer.do_capture(move, board)
+      end
+    end
+
+    context "when the target square does not have an opponent piece" do
+      it "does not send do_move, then returns false" do
+        allow(board).to receive(:at_sq).with(move.target).and_return nil
+
+        expect(computer).not_to receive(:do_move)
+        computer.do_capture(move, board)
+      end
+    end
+  end # describe '#do_capture'
 end
