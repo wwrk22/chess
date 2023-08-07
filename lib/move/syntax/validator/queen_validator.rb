0ddcfdb737db254f73a692a5ee2c43abf5ee1284
Won Rhim <wwrk22@gmail.com>
2023-08-07 12:27:21 -0400

    refactor: redo QueenMoves module namespacing

--- lib/move/syntax/validator/queen_validator.rb
+++ lib/move/syntax/validator/queen_validator.rb
@@ -1,4 +1,4 @@
-require_relative '../move/queen_moves'
+require_relative '../pattern/queen'
 require './lib/piece/chess_piece'
 require './lib/piece/piece_specs'
 require './lib/error/color_unknown_error'
@@ -6,11 +6,10 @@ require './lib/error/color_unknown_error'
 
 class QueenValidator
   include PieceSpecs
+  include MoveSyntax::Queen
 
   # Return the move if move has valid syntax. Otherwise, return nil.
-  # Raise ColorUnknownError if color is unknown.
   def validate(move_str, color)
-    raise ColorUnknownError.new(color) if valid_color?(color) == false
-    ChessPiece.new(queen, color) if move_str =~ QueenMoves::MOVE
+    ChessPiece.new(queen, color) if move_str =~ queen_move_syntax
   end
 end
