50e486124de626250070328d025240fc508e9197
Won Rhim <wwrk22@gmail.com>
2023-08-10 21:42:39 -0400

    feat: update MovePerformer#do_move to handle all move types

--- lib/move/performer/move_performer.rb
+++ lib/move/performer/move_performer.rb
@@ -1,21 +1,43 @@
-class MovePerformer
+require './lib/piece/piece_specs'
+require './lib/board/board'
 
-  ##
-  # Perform the move on the board. Return true if the move was performed, or
-  # false if it could not be performed.
+
+class MovePerformer
+  include PieceSpecs
+
   def do_move(move, board)
-    return false if not board.at(move.target).nil?
-    board.set(move.start)
-    board.set(move.target, move.piece)
-    true
+    return do_ep_move(move, board) if move.ep
+
+    target = board.at(move.target)
+
+    if move.capture
+      if target && target.color != move.piece.color
+        board.set(move.target, move.piece)
+        board.set(move.start)
+        return true
+      end
+    else # not capture
+      if target.nil?
+        board.set(move.target, move.piece)
+        board.set(move.start)
+        return true
+      end
+    end
+
+    return false
   end
 
-  ##
-  # Perform the capture move on the board. Return true if the move was
-  # performed, or false if it could not be performed.
-  def do_capture(move, board)
-    target = board.at(move.target)
-    (target && target.color != move.piece.color) ?
-      do_move(move, board) : false
+  def do_ep_move(move, board)
+    target_square = board.at(move.target)
+    ep_pawn = board.at(move.ep_sq)
+
+    if target_square.nil? && ep_pawn && ep_pawn.type == pawn && ep_pawn.color != move.piece.color
+      board.set(move.target, move.piece)
+      board.set(move.start)
+      board.set(move.ep_sq)
+      return true
+    else
+      return false
+    end
   end
 end
